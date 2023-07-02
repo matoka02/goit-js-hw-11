ca3a9a4eaff9f56843969a6789ebfcb4c39cb9c4
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-02 21:35:00 +0300

    update

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,9 +1,9 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '37972717-70d116d5c7dba3fcb6f3ce7e2';
 
-async function fetchImages(inputValue, pageNr) {
+async function fetchImages(inputValue, pageNumber) {
     return await fetch(
-        `${BASE_URL}?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pageNr}`
+        `${BASE_URL}?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pageNumber}`
     )
         .then(async resp => {
             if (!resp.ok) {
@@ -12,6 +12,7 @@ async function fetchImages(inputValue, pageNr) {
                 }
                 throw new Error(resp.status);
             }
+            // console.log(resp);      
             return await resp.json();
         })
         .catch(error => {
@@ -19,4 +20,4 @@ async function fetchImages(inputValue, pageNr) {
         });
 };
 
-export {fetchImages}
+export {fetchImages};
--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,13 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const input = document.querySelector('.search-form-input');
-const btnSearch = document.querySelector('.search-form-button');
+const buttonSearch = document.querySelector('.search-form-button');
 const gallery = document.querySelector('.gallery');
-const btnLoadMore = document.querySelector('.load-more');
+const buttonLoadMore = document.querySelector('.load-more');
+
 let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 
+// нужно для SimpleLightbox
 // const { height: cardHeight } = document
 //     .querySelector('.gallery')
 //     .firstElementChild.getBoundingClientRect();
@@ -19,82 +21,70 @@ let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 //     behavior: 'smooth',
 // });
 
-btnLoadMore.style.display = 'none';
+buttonLoadMore.style.display = 'none';
 
 let pageNumber = 1;
 
-btnSearch.addEventListener('click', e => {
-    e.preventDefault();
+buttonSearch.addEventListener('click', (evt) => {
+    evt.preventDefault();
     cleanGallery();
-    const trimmedValue = input.value.trim();
-    if (trimmedValue !== '') {
-        fetchImages(trimmedValue, pageNumber).then(foundData => {
+
+    const inputValue = input.value.trim();
+    console.log(typeof inputValue);         // string
+
+    if (inputValue !== '') {
+        fetchImages(inputValue, pageNumber).then(foundData => {
             if (foundData.hits.length === 0) {
-                Notiflix.Notify.failure(
-                    'Sorry, there are no images matching your search query. Please try again.'
-                );
+                Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
             } else {
-                renderImageList(foundData.hits);
-                Notiflix.Notify.success(
-                    `Hooray! We found ${foundData.totalHits} images.`
-                );
-                btnLoadMore.style.display = 'block';
+                // console.log(typeof foundData);       // object
+                createMarkup(foundData.hits);
+                Notiflix.Notify.success(`Hooray! We found ${foundData.totalHits} images.`);
+                buttonLoadMore.style.display = 'block';
                 gallerySimpleLightbox.refresh();
             }
         });
     }
 });
 
-btnLoadMore.addEventListener('click', () => {
-    pageNumber++;
-    const trimmedValue = input.value.trim();
-    btnLoadMore.style.display = 'none';
-    fetchImages(trimmedValue, pageNumber).then(foundData => {
+
+buttonLoadMore.addEventListener('click', (evt) => {
+    pageNumber+=1;
+    buttonLoadMore.style.display = 'none';
+
+    const inputValue = input.value.trim();
+
+    fetchImages(inputValue, pageNumber).then(foundData => {
         if (foundData.hits.length === 0) {
-            Notiflix.Notify.failure(
-                'Sorry, there are no images matching your search query. Please try again.'
-            );
+            Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
         } else {
-            renderImageList(foundData.hits);
-            Notiflix.Notify.success(
-                `Hooray! We found ${foundData.totalHits} images.`
-            );
-            btnLoadMore.style.display = 'block';
+            createMarkup(foundData.hits);
+            Notiflix.Notify.success(`Hooray! We found ${foundData.totalHits} images.`);
+            buttonLoadMore.style.display = 'block';
         }
     });
 });
 
-function renderImageList(images) {
-    console.log(images, 'images');
-    const markup = images
-        .map(image => {
-            console.log('img', image);
+function createMarkup(images) {
+    console.log(images);
+    const markup = images.map(image => {
+            console.log(image);
             return `<div class="photo-card">
-
-             <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
-
+                <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
                 <div class="info">
-                     <p class="info-item">
-        <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
-</p>
-                        <p class="info-item">
-                                <b>Views</b> <span class="info-item-api">${image.views}</span>    
-                        </p>
-                        <p class="info-item">
-                                <b>Comments</b> <span class="info-item-api">${image.comments}</span>    
-                        </p>
-                        <p class="info-item">
-                                <b>Downloads</b> <span class="info-item-api">${image.downloads}</span> 
-                        </p>
+                    <p class="info-item"><b>Likes</b> <span class="info-item-api">${image.likes}</span></p>
+                    <p class="info-item"><b>Views</b> <span class="info-item-api">${image.views}</span></p>
+                    <p class="info-item"><b>Comments</b> <span class="info-item-api">${image.comments}</span></p>
+                    <p class="info-item"><b>Downloads</b> <span class="info-item-api">${image.downloads}</span></p>
                 </div>
-        </div>`;
+            </div>`;
         })
         .join('');
     gallery.innerHTML += markup;
-}
+};
 
 function cleanGallery() {
     gallery.innerHTML = '';
     pageNumber = 1;
-    btnLoadMore.style.display = 'none';
-}
+    buttonLoadMore.style.display = 'none';
+};
